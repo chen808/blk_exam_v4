dc791e6b4b7714b0269bddb2bd0ca2fbc438be1d
David Chen <chentiki808@gmail.com>
2016-04-15 10:45:35 -0700

    initial commit

--- server/config/routes.js
+++ server/config/routes.js
@@ -8,35 +8,11 @@ var question = require('./../controllers/questions.js');
 module.exports = function(app){
 
 	// GETS ====================================================
-	app.get('/getAllQuestions', function(req, res){
-		question.get_all_question(req, res);
-	})
 
-	app.get('/getQuestion/:id', function(req, res){
-		question.get_one_question(req, res, req.params.id);
-	})
 
-	app.get('/getAll/:id', function(req, res){
-		question.get_all(req, res, req.params.id);
-	})
 
 
 	// POSTS ===================================================
-
-	// req: adds new question
-	// res: return the success message
-	app.post('/newQuestion', function(req, res){
-		question.new_Question(req, res);
-	})
-
-	app.post('/answer_this', function(req, res){
-		question.answer_to_question(req, res);
-	})
-
-	app.get('/updateLike/:id', function(req, res){
-		console.log(req.params.id);
-		question.update_like(req, res, req.params.id);
-	})
 
 
 
--- server/controllers/questions.js
+++ server/controllers/questions.js
@@ -6,101 +6,10 @@ module.exports = (function(){
 
 	return{
 
-		new_Question: function(req, res){
-			Question.create({
-				user_name:req.body.user_name,
-				question:req.body.question,
-				detail:req.body.detail
-			}, function(err, results){
-				if(err){
-					console.log("Error adding question");
-				}
-				else{
-					//console.log("Success adding question!");
-					// returning success sign
-					return res.json({success:"Successfully added question!"});
-				}
-			})
-		},
 
 
 
-		get_all_question: function(req, res){
-			Question.find({}, function(err, results){
-				if(err){
-					console.log('Error getting data');
-				}
-				else{
-					res.json(results);
-				}
-			})
-		},
 
-
-		get_one_question: function(req, res){
-			Question.findOne({_id:req.params.id}, function(err, results){
-				if(err){
-					console.log('error getting question');
-				}
-				else{
-					res.json(results);
-				}
-			})
-		},
-
-		answer_to_question: function(req, res){
-			// find the question
-			Question.findOne({_id:req.body.questionID}, function(err, question){
-				var answer = new Answer({user_name:req.body.user_name, answer:req.body.answer, support_detail:req.body.support_detail});
-
-				// increment the answer count
-				Question.findOneAndUpdate( {_id:req.body.questionID}, {$inc:{answer_count: 1}}, function(err, Question){
-	 				if(err){
-	 					console.log('error updating post count');
-	 				}
-	 				else{
-	 					console.log('successfully updated post count');
-	 				}
-	 			})
-
-				answer._question = question._id;
-
-				question.answers.push(answer);
-
-				answer.save(function(err){
-					question.save(function(err){
-						if(err){
-							console.log('error');
-						}
-						else{
-							console.log('successfully saved answer!')
-						}
-					})
-				})
-			})
-			
-		},
-
-		get_all: function(req, res){
-			Question.findOne({_id:req.params.id})
-				.populate('answers')
-				.exec(function(err, answer){
-					res.json('answer', {answer});
-				})
-
-		},
-
-
-		update_like: function(req, res){
-			Answer.findOneAndUpdate( {_id:req.params.id}, {$inc:{likes: 1}}, function(err, Answer){
-	 				if(err){
-	 					console.log('error updating like count');
-	 				}
-	 				else{
-	 					console.log('successfully updated like count');
-	 				}
-	 			})
-		}
 
 
 
